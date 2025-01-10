0b24b36bba9a57ff8b6b9edbd043e0f9b6d65334
Oles <gedzoles@gmail.com>
2025-01-11 00:59:27 +0200

    data

---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,40 @@
+using System;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    private Vector3 targetPosition;
+    [SerializeField] private Animator animator;
+    
+    
+    private void Awake()
+    {
+        targetPosition = transform.position;
+    }
+    
+    public void Move(Vector3 targetPosition)
+    {
+        this.targetPosition = targetPosition;
+    }
+
+    private void Update()
+    {
+        
+        const float moveSpeed = 5f;
+        const float minDistance = 0.1f;
+
+        if (Vector3.Distance(transform.position, targetPosition) > minDistance)
+        {
+            var direction = (targetPosition - transform.position).normalized;
+            transform.position += direction * (moveSpeed * Time.deltaTime);
+            
+            const float rotationSpeed = 10f;
+            transform.forward = Vector3.Lerp(transform.forward, direction, Time.deltaTime * rotationSpeed);
+            animator.SetBool("isWalking", true);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
+    }
+}
---
+++ Assets/Scripts/UnitActionSystem.cs
@@ -0,0 +1,65 @@
+using System;
+using UnityEngine;
+
+public class UnitActionSystem : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public static UnitActionSystem Instance { get; private set; }
+
+    public event EventHandler OnSelectedUnitChanged;
+
+    [SerializeField] private Unit selectedUnit;
+    [SerializeField] private LayerMask unitLayerMask;
+
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogError("There should never be two UnitActionSystem." + transform + " - " + Instance);
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!TryHandleUnitsSelection())
+            {
+                selectedUnit.Move(MouseWorld.GetPosition());
+            }
+        }
+    }
+
+    private bool TryHandleUnitsSelection()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, unitLayerMask))
+        {
+            if (raycastHit.transform.TryGetComponent<Unit>(out Unit unit))
+            {
+                SetSelectedUnit(unit);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+    private void SetSelectedUnit(Unit unit)
+    {
+        selectedUnit = unit;
+        OnSelectedUnitChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+    public Unit GetSelectedUnit()
+    {
+        return selectedUnit;
+    }
+}
